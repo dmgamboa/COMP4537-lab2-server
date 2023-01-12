eae4bb217f90372a8f6c078e95caea321c6a9039
Donna Dominguez Gamboa <dm.dominguezgamboa@gmail.com>
2023-01-12 12:02:24 -0800

    Add node fetch

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 const cors = require('cors');
 const PORT = 3000 || process.env.PORT;
 
