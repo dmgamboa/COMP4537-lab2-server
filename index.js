4d079911ad3e36f43b9c15ce4061c36ba502b2d0
Donna Dominguez Gamboa <dm.dominguezgamboa@gmail.com>
2023-01-12 11:19:29 -0800

    Add code

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fetch = require('node-fetch');
+
+const PORT = 3000 || process.env.PORT;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors({ origin: '*' }));
+
+app.post('/chatbot', (req, res) => {
+    const message = req.body.message;
+    console.log('body', req.body);
+
+    const number = message?.toString().match(/\d+/);
+
+    if (number) {
+		fetch(`http://numbersapi.com/${number}?type=trivia`)
+        .then(response => response.text())
+        .then(data => {
+			res.json({
+				text: data
+			});
+		}).catch(error => {
+			res.json({
+				text: "Sorry, I couldn't find any information about that number."
+			});
+		});
+	} else {
+        res.json({
+            text: "I'm sorry, I don't undestand your question. Please ask me about a number."
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
